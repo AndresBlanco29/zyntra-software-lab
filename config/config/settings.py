41578671d769d7b99e80b8a4fd5b4656e927e484
jam8106 <jam8106@hotmail.com>
2026-03-04 17:03:31 -0500

    Avance Login

--- config/config/settings.py
+++ config/config/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tortilla_db',
         'USER': 'root',
-        'PASSWORD': '030624',
+        'PASSWORD': '010409',
         'HOST': 'localhost',
         'PORT': '3306',
     }
@@ -131,7 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- config/config/urls.py
+++ config/config/urls.py
@@ -21,12 +21,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
+
+
 urlpatterns = [
 
     path('admin/', admin.site.urls),
 
     path('', include('productos.urls')),
 
+    path('', include('usuarios.urls')),
 ]
 
 if settings.DEBUG:

aba5ccf80182bbf533f8ca82c173b07a7065340a
AndresBlanco29 <andresilloblanco29@gmail.com>
2026-03-23 22:51:24 -0500

    test endpoint directo

--- config/config/urls.py
+++ config/config/urls.py
@@ -1,56 +1,9 @@
-"""
-URL configuration for config project.
+from django.http import HttpResponse
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.conf.urls.i18n import i18n_patterns
-from django.views.static import serve
-
-from django.conf import settings
-from django.conf.urls.static import static
-from core.views import home
-
-
-
+def test(request):
+    return HttpResponse("FUNCIONA")
 
 urlpatterns = [
-
-    path('admin/', admin.site.urls),
-
-    path('', home),
-
-    path('productos/', include('productos.urls')),
-    
-    path('cotizaciones/', include('cotizaciones.urls')),
-
-    path('carrito/', include('carrito.urls')),
-
-    path('vendedores/', include('vendedores.urls')),
-
-    path('usuarios/', include('usuarios.urls')),
-
-    path('clientes/', include('clientes.urls')),
-
-    path("i18n/", include("django.conf.urls.i18n")),
-
+    path('', test),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-elif getattr(settings, 'SERVE_MEDIA', False):
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]

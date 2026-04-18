944019b6dddb74079d67a95d2b117c5cb05831c9
AndresBlanco29 <andresilloblanco29@gmail.com>
2026-04-17 19:19:59 -0500

    TERCERA ENTREGA - ARREGLO V6

--- config/usuarios/schema_repair.py
+++ config/usuarios/schema_repair.py
@@ -1,9 +1,10 @@
 import logging
 import threading
+from copy import copy
 
 from django.apps import apps
 from django.db import connections
-from django.db.utils import OperationalError, ProgrammingError
+from django.db.utils import IntegrityError, OperationalError, ProgrammingError
 
 
 logger = logging.getLogger(__name__)
@@ -38,6 +39,60 @@ def _iter_missing_concrete_fields(connection, model):
         yield field
 
 
+def _build_relaxed_field(field):
+    relaxed_field = copy(field)
+    relaxed_field._unique = False
+    relaxed_field.null = True
+    relaxed_field.blank = True
+    relaxed_field.default = None
+    return relaxed_field
+
+
+def _backfill_field_values(connection, model, field):
+    table_name = model._meta.db_table
+    pk_column = model._meta.pk.column
+    column_name = field.column
+
+    with connection.cursor() as cursor:
+        cursor.execute(
+            f"SELECT `{pk_column}` FROM `{table_name}` WHERE `{column_name}` IS NULL"
+        )
+        primary_keys = [row[0] for row in cursor.fetchall()]
+
+    for primary_key in primary_keys:
+        value = field.get_default()
+        prepared_value = field.get_db_prep_save(value, connection)
+        with connection.cursor() as cursor:
+            cursor.execute(
+                f"UPDATE `{table_name}` SET `{column_name}` = %s WHERE `{pk_column}` = %s",
+                [prepared_value, primary_key],
+            )
+
+
+def _add_missing_field(connection, model, field):
+    if field.unique and not field.primary_key:
+        relaxed_field = _build_relaxed_field(field)
+        with connection.schema_editor() as schema_editor:
+            schema_editor.add_field(model, relaxed_field)
+
+        _backfill_field_values(connection, model, field)
+
+        try:
+            with connection.schema_editor() as schema_editor:
+                schema_editor.alter_field(model, relaxed_field, field, strict=False)
+        except IntegrityError as exc:
+            logger.warning(
+                "Runtime schema repair left %s.%s without unique constraint after backfill: %s",
+                model._meta.db_table,
+                field.column,
+                exc,
+            )
+        return
+
+    with connection.schema_editor() as schema_editor:
+        schema_editor.add_field(model, field)
+
+
 def ensure_runtime_schema():
     global _schema_repair_completed, _schema_repair_running
 
@@ -63,8 +118,7 @@ def ensure_runtime_schema():
                 continue
 
             for field in _iter_missing_concrete_fields(connection, model):
-                with connection.schema_editor() as schema_editor:
-                    schema_editor.add_field(model, field)
+                _add_missing_field(connection, model, field)
 
                 logger.warning(
                     "Runtime schema repair added missing %s.%s column",
